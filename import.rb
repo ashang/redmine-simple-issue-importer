0dc3a6af5f1a0a8a942e0aac5d4c519a4e3c9cb4
Jan Thurau <jan@janthurau.de>
2013-11-06 10:15:25 +0100

    Moves api key to the top of the file

--- import.rb
+++ import.rb
@@ -7,12 +7,14 @@ csvFile = "data.csv"
 host = '10.55.10.13'
 port = '3000'
 path = '/issues.json'
+key = 'e3dad26b1b87228ca344506a87fa936b2444e569' # redmine api key
 
 
 class RedmineImport
-  def initialize(host,port)
+  def initialize(host,port, key)
     @host = host
     @port = port
+    @key = key
   end
   
   def readCsv(filename)
@@ -27,7 +29,7 @@ class RedmineImport
     
   end
   
-  def import()
+  def importIssues()
     
     @csvData.each{ |row| 
       sendRequest('/issues.json', buildRequest(row))
@@ -37,7 +39,7 @@ class RedmineImport
   
   def buildRequest(row)
     body = ActiveSupport::JSON.encode({
-      :key => "e3dad26b1b87228ca344506a87fa936b2444e569",
+      :key => @key,
       :issue => {
         :project_id => 7,
         :tracker_id => 6,
@@ -71,53 +73,6 @@ class RedmineImport
 end
 
 
-ri = RedmineImport.new(host,port)
+ri = RedmineImport.new(host,port,key)
 ri.readCsv(csvFile)
-ri.import()
-
-
-
-exit
-
-
-CSV.foreach(csvFile, {
-  :col_sep => ';',
-  :headers => true
-}) do |row|
-
-  body = ActiveSupport::JSON.encode({
-    :key => "e3dad26b1b87228ca344506a87fa936b2444e569",
-    :issue => {
-      :project_id => 7,
-      :tracker_id => 6,
-      :status_id => 1,
-      #:priority_id => 0,
-      :subject => row['Subject'],
-      :description => '',
-      #:category_id => 0,
-      :assigned_to_id => 0,
-      :custom_fields => [
-        { :id => 3, :value => row['E-Mail']}, # E-Mail
-        { :id => 4, :value => row['Website']}, # Website
-        { :id => 5, :value => row['Contact Person']}, # Contact Person
-        { :id => 6, :value => row['Country']}, # Country
-        { :id => 7, :value => 'Some contract information'}, # Contract Information
-        { :id => 8, :value => row['Address']}, # Address
-        { :id => 9, :value => row['Phone']} # Phone
-      ]
-    }
-  })
-
-
-
-  request = Net::HTTP::Post.new(path, initheader = {'Content-Type' =>'application/json'})
-  request.body = body
-  response = Net::HTTP.new(host, port).start {|http| http.request(request) }
-  puts "Response #{response.code} #{response.message}: #{response.body}"
-
-
-end
-
-# Subject;Assignee;Status;Country;Contact Person;E-Mail;Phone;Website;Address
-
-
+ri.importIssues()
